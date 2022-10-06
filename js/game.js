80c14b15ea12e04c15309ebc5ee45bc9f22ee183
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-06 00:59:59 -0300

    Create function to block user input

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
+
+function blockInputUser() {
+    for(let i = 1; i < (cards.length + 1); i++) {
+        let $cards = document.querySelector(`#${cards[i - 1]}`);
+        $cards.removeEventListener('click', handleClicks);        
+    }
+}
+
 function resetEasyDifficultCards() {
     for(let i = 1; i < (cards.length + 1); i++) {
         document.querySelector(`${correctEven[i - 1]}`).id = `easy-card-n${i}`;
