df5076eb2af4ce15b59ad5ec0c616000a48176cd
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-10-06 01:01:31 -0300

    Create function to turn back a card if it isn't even

--- js/game.js
+++ js/game.js
@@ -1,4 +1,9 @@
 
+function backCard() {
+    document.querySelector(firstCardChoosed).className = content;
+    document.querySelector(secondCardChoosed).className = content;
+}
+
 function blockInputUser() {
     for(let i = 1; i < (cards.length + 1); i++) {
         let $cards = document.querySelector(`#${cards[i - 1]}`);
